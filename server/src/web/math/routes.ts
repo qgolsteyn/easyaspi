faacb725a1647a47e6411d0781dbe4c5926542e1
Arjun Dhadda <arjundhadda236@gmail.com>
2019-10-19 17:28:40 -0700

    commenting out a dummy endpoint in for posting math problems. this one will never be used but I still want to reference the code for later. Will be removed soon

--- server/src/web/math/routes.ts
+++ server/src/web/math/routes.ts
@@ -20,25 +20,25 @@ export const initializeMathRoutes = (app: express.Application) => {
         }
     });
 
-    /* post a problem (for putting dummy data to DB through API) */
-    mathRouter.post('/problem', async (req, res) => {
-        const problem = problemSerializer.parse(req.body);
-        if (problem) {
-            try {
-                const p = new GeneratedProblemModel(problem);
-                await p.save();
-                res.status(200);
-                res.json(problem);
-            } catch (e) {
-                console.error(e);
-                res.status(500);
-                res.send('Error 500');
-            }
-        } else {
-            res.status(400);
-            res.send('Invalid request');
-        }
-    });
+    // /* post a problem (for putting dummy data to DB through API) */
+    // mathRouter.post('/problem', async (req, res) => {
+    //     const problem = problemSerializer.parse(req.body);
+    //     if (problem) {
+    //         try {
+    //             const p = new GeneratedProblemModel(problem);
+    //             await p.save();
+    //             res.status(200);
+    //             res.json(problem);
+    //         } catch (e) {
+    //             console.error(e);
+    //             res.status(500);
+    //             res.send('Error 500');
+    //         }
+    //     } else {
+    //         res.status(400);
+    //         res.send('Invalid request');
+    //     }
+    // });
 
     /* get all the math problem (for now) */
     //todo implement logic for getting a single math problem
