7527686ae9604dbfeb7f7256867670dd369f2dd6
Noor <noor_khan@alumni.ubc.ca>
2019-10-20 21:51:28 -0700

    feature/push-notification

--- server/src/web/index.ts
+++ server/src/web/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 import { initializeMathRoutes } from './math/routes';
 import { initializeUsersRoutes } from './users/routes';
+import { initializeNotificationRoutes } from './notification/routes';
 
 export const initializeRoutes = (app: express.Application) => {
     initializeMathRoutes(app);
     initializeUsersRoutes(app);
+    initializeNotificationRoutes(app);
 };
