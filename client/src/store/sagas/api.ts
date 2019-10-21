61a7b7e952e485000f62c74d60e6f4d8281904a7
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-10-21 08:34:43 -0700

    Fix merge conflict

--- client/src/store/sagas/api.ts
+++ client/src/store/sagas/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+<<<<<<< Updated upstream
 const URL = 'http://localhost:3000';
 
 export const api = axios.create({
     baseURL: 'http://localhost:3000',
+=======
+export const baseApi = axios.create({
+    baseURL: 'http://192.168.0.107:3000',
+>>>>>>> Stashed changes
 });
