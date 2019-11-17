29a1f4aed347d92ef7242f60f4d4b0d4acc6d694
Arjun Dhadda <arjundhadda236@gmail.com>
2019-11-16 21:31:04 -0800

    supporting subtraction problem types now

--- server/src/service/math/mathService.ts
+++ server/src/service/math/mathService.ts
@@ -7,9 +7,22 @@ const log = debug('pi:route');
 
 export const fetchNextMathProblem = async () => {
     // call next problem algo using user object
-    const supportedTypes = [ProblemType.ADDITION];
-    const difficulties = [
+    const supportedTypes = [ProblemType.ADDITION, ProblemType.SUBTRACTION];
+    const additionDiffs = [
         ProblemDifficulty.G1E,
+        ProblemDifficulty.G1M,
+        ProblemDifficulty.G1H2E,
+        ProblemDifficulty.G2M,
+        ProblemDifficulty.G2H3E,
+        ProblemDifficulty.G3M,
+        ProblemDifficulty.G3H4E,
+        ProblemDifficulty.G4M,
+        ProblemDifficulty.G4H5E,
+        ProblemDifficulty.G5M,
+        ProblemDifficulty.G5H,
+    ];
+
+    const subDiffs = [
         ProblemDifficulty.G1M,
         ProblemDifficulty.G1H2E,
         ProblemDifficulty.G2M,
@@ -24,17 +37,25 @@ export const fetchNextMathProblem = async () => {
 
     // for now just randomize this stuff, later on we need to use next problem algo
     const supportedTypesIndex = generateNumber(0, supportedTypes.length - 1);
-    const difficultiesIndex = generateNumber(0, difficulties.length - 1);
+
+    let difficulty: ProblemDifficulty;
+    if (supportedTypes[supportedTypesIndex] === ProblemType.ADDITION) {
+        const difficultiesIndex = generateNumber(0, additionDiffs.length - 1);
+        difficulty = additionDiffs[difficultiesIndex];
+    } else {
+        const difficultiesIndex = generateNumber(0, subDiffs.length - 1);
+        difficulty = subDiffs[difficultiesIndex];
+    }
 
     log(
         'problemType: ' +
             supportedTypes[supportedTypesIndex] +
             ' difficulty: ' +
-            difficulties[difficultiesIndex],
+            difficulty,
     );
 
     const problem = await generateArithmeticProblem(
-        difficulties[difficultiesIndex],
+        difficulty,
         supportedTypes[supportedTypesIndex],
     );
 
