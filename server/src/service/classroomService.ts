35949de0cf85e92fab9f16e60e8bb3215786a499
Noor <noor_khan@alumni.ubc.ca>
2019-11-18 17:50:50 -0800

    classroom get and update

--- server/src/service/classroomService.ts
+++ server/src/service/classroomService.ts
@@ -1,7 +1,8 @@
 import Boom from 'boom';
 
 import { ClassroomModel, UserModel } from '@server/database';
-import { IClassroom, UserType } from '@shared/index';
+import { IClassroom, ProblemType, UserType } from '@shared/index';
+import { convertStringToProblemType } from '@shared/models/problem';
 
 export const createClassroom = async (classroomPayload: IClassroom) => {
     const classroom = new ClassroomModel({
@@ -22,6 +23,7 @@ export const createClassroom = async (classroomPayload: IClassroom) => {
 export const authenticateToClassroom = async (classroomPayload: IClassroom) => {
     const classroom = await ClassroomModel.findOne({
         name: classroomPayload.name,
+        passcode: classroomPayload.passcode,
     });
 
     if (classroom && classroom.passcode === classroomPayload.passcode) {
@@ -38,4 +40,37 @@ export const getStudents = async (classroomId: string) => {
     });
 
     return students;
+};
+
+export const getClassroom = async (classroomId: string) => {
+    const classroom = await ClassroomModel.findById(classroomId);
+
+    if(!classroom){
+        throw Boom.notFound(`No classroom found with the id ${classroomId}`);
+    }
+
+    return classroom;
+};
+
+export const updateClassroom = async (classroomPayload: IClassroom) => {
+
+    if(classroomPayload.problemsForToday.length !== 0){
+        for(const problemTypeStr of classroomPayload.problemsForToday){
+            const problemType = convertStringToProblemType(problemTypeStr);
+            if (problemType === ProblemType.UNKNOWN){
+                throw Boom.badData(`${problemTypeStr} is not a valid problemType`)
+            }
+        }
+    }
+
+    const classroom = await ClassroomModel.updateOne({
+            name: classroomPayload.name,
+        passcode: classroomPayload.passcode,
+    }, classroomPayload);
+
+    if(!classroom){
+        throw Boom.notFound('Could not update the classroom');
+    }
+
+    return classroom;
 };
--- server/src/web/classroomRoutes.ts
+++ server/src/web/classroomRoutes.ts
@@ -1,14 +1,16 @@
 import Boom from 'boom';
 import express from 'express';
 
+import { ClassroomModel } from '@server/database';
 import { classroomService } from '@server/service';
 import { enhanceHandler, HTTP_CODE } from '@server/service/utils/routeEnhancer';
+import { convertStringToProblemType, ProblemType } from '@shared/models/problem';
 
 export const initializeClassroomRoutes = (app: express.Application) => {
-    const mathRouter = express.Router();
-    app.use('/classroom', mathRouter);
+    const classroomRouter = express.Router();
+    app.use('/classroom', classroomRouter);
 
-    mathRouter.get(
+    classroomRouter.get(
         '/students',
         enhanceHandler({ protect: true })(async (_, user) => {
             if (user && user.virtualClassroomUid) {
@@ -19,6 +21,40 @@ export const initializeClassroomRoutes = (app: express.Application) => {
             } else {
                 throw Boom.unauthorized();
             }
-        }),
+        })
+    );
+
+    classroomRouter.put(
+        '/', enhanceHandler({ protect: true })(async (req, user) => {
+
+            if(!req.body || !req.body.name || !req.body.passcode){
+                throw Boom.badRequest('Invalid Payload');
+            }
+
+            if(!req.body.problemsForToday && req.body.problemsForToday.length !== 0){
+                for(const problemTypeStr of req.body.problemsForToday){
+                    const problemType = convertStringToProblemType(problemTypeStr);
+                    if (problemType === ProblemType.UNKNOWN){
+                        throw Boom.badData(`${problemTypeStr} is not a valid problemType`)
+                    }
+                }
+            }
+
+            if (typeof user === 'undefined'){
+                throw Boom.badData('user can not be undefined');
+            }
+
+            const classroomId = user.virtualClassroomUid;
+
+            const classroom = await ClassroomModel.findByIdAndUpdate({
+                classroomId
+            }, req.body);
+
+            if(!classroom){
+                throw Boom.notFound('Could not update the classroom');
+            }
+
+            return [HTTP_CODE.OK, classroom];
+        })
     );
 };
