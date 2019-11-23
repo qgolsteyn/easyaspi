de9a1fdb5440490d4cdc32a826a179aa5fb7eaa1
ArjunDhadda <arjundhadda236@gmail.com>
2019-11-23 02:28:29 -0800

    Feature/making incorrect solutions trickier (#75)
    * incorrect solutions are now trickier to distinguish from correct solutions
    
    * fixing linting issues
    
    * formatting issue

--- server/src/service/math/generateNumbers.ts
+++ server/src/service/math/generateNumbers.ts
@@ -1,4 +1,8 @@
-const INCORRECT_SOLUTIONS_VARIANCE = 0.1;
+const INCORRECT_SOLUTIONS_MIN_VARIANCE = 0.05;
+const INCORRECT_SOLUTIONS_MAX_VARIANCE = 0.1;
+
+const NUMBER_OF_INCORRECT_SOLUTIONS = 3;
+const SAME_LAST_DIGIT_THRESHOLD = 20;
 
 export const generateNumber = (
     min: number,
@@ -20,39 +24,130 @@ export const generateIncorrectWholeNumberSolutions = (
     solution: number,
     multiplesOf?: number,
 ) => {
-    const incorrectSolutions = [];
+    const incorrectSolutions: string[] = [];
 
     if (solution === 0) {
         incorrectSolutions.push('1', '2', '3');
     } else {
-        // rounded up value of 10%
-        let diff = Math.ceil(solution * INCORRECT_SOLUTIONS_VARIANCE);
+        // generate 3 incorrect solutions but randomize the number that are
+        // greater than or less than the solution
+        let numValuesGreaterThanSolution = generateNumber(
+            0,
+            NUMBER_OF_INCORRECT_SOLUTIONS,
+        );
+        const numValuesLessThanSolution =
+            NUMBER_OF_INCORRECT_SOLUTIONS - numValuesGreaterThanSolution;
 
-        if (multiplesOf && multiplesOf > 1) {
-            while (diff % multiplesOf !== 0) {
-                diff++;
+        // keep track of a count and apply it to the difference to avoid duplicates
+        let count = 1;
+        for (let i = 0; i < numValuesLessThanSolution; i++) {
+            const diff = generateDifference(solution, multiplesOf);
+
+            // if subtracting the difference from the solution results in 0 or
+            // negative, ensure an extra incorrect solution greater than solution
+            // is generated
+            if (solution - count * diff < 1) {
+                numValuesGreaterThanSolution++;
+            } else {
+                incorrectSolutions.push(
+                    generateIncorrectValue(
+                        diff,
+                        incorrectSolutions,
+                        solution - count * diff,
+                        solution,
+                    ),
+                );
             }
+            count++;
         }
 
-        const two = 2;
-        const three = 3;
+        count = 1;
+        for (let i = 0; i < numValuesGreaterThanSolution; i++) {
+            // difference from solution to use for incorrect solution
+            const diff = generateDifference(solution, multiplesOf);
 
-        // pretty simple incorrect solutions just take ~10% differences
-        if (solution - diff < 1) {
+            // generate the incorrect solution string and push it to the array
             incorrectSolutions.push(
-                String(solution + diff),
-                String(solution + two * diff),
-                String(solution + three * diff),
+                generateIncorrectValue(
+                    diff,
+                    incorrectSolutions,
+                    solution + count * diff,
+                    solution,
+                ),
             );
-        } else {
-            incorrectSolutions.push(
-                String(solution - diff),
-                String(solution + diff),
-                String(solution + two * diff),
-            );
+            count++;
         }
     }
     return incorrectSolutions;
+};
+
+const generateDifference = (solution: number, multiplesOf?: number) => {
+    // find the percentage variance to take from the solution
+    const variance =
+        Math.random() *
+            (INCORRECT_SOLUTIONS_MAX_VARIANCE -
+                INCORRECT_SOLUTIONS_MIN_VARIANCE) +
+        INCORRECT_SOLUTIONS_MIN_VARIANCE;
+
+    let diff = Math.ceil(solution * variance);
+
+    // make sure variance conforms to the multiplesOf constriction
+    if (multiplesOf && multiplesOf > 1) {
+        while (diff % multiplesOf !== 0) {
+            diff++;
+        }
+    }
+
+    return diff;
+};
+
+const generateIncorrectValue = (
+    difference: number,
+    incorrectSolutions: string[],
+    incorrectValue: number,
+    solution: number,
+) => {
+    // only try to generate incorrect value with same last digit if solution is above 20
+    if (incorrectValue > SAME_LAST_DIGIT_THRESHOLD) {
+        const incorrectValueString = incorrectValue.toString();
+
+        const ten = 10;
+        // try to generate an incorrect solution with the same last digit as solution to make
+        // multiple choice harder
+        const sameLastDigitIncorrectValueString =
+            incorrectValueString.substring(0, incorrectValueString.length - 1) +
+            (solution % ten);
+
+        const sameLastDigitIncorrectValue = parseInt(
+            sameLastDigitIncorrectValueString,
+            ten,
+        );
+
+        // if incorrect solution with same last digit equals the solution or already exists
+        // within the incorrect solutions array, then it cannot be used
+        if (
+            sameLastDigitIncorrectValue === solution ||
+            incorrectSolutions.includes(sameLastDigitIncorrectValueString)
+        ) {
+            // cannot allow duplicate values in incorrect solutions array
+            while (incorrectSolutions.includes(incorrectValue.toString())) {
+                incorrectValue += difference;
+            }
+            return incorrectValue.toString();
+        } else {
+            return sameLastDigitIncorrectValueString;
+        }
+    } else {
+        // cannot allow duplicate values in incorrect solutions array
+        while (
+            solution === incorrectValue ||
+            incorrectSolutions.includes(incorrectValue.toString())
+        ) {
+            incorrectValue += difference;
+        }
+
+        return incorrectValue.toString();
+    }
 };
 
 export const generateIncorrectWholeNumberSolutionsWithRemainder = (
