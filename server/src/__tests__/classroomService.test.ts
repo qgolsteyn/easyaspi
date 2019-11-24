50d1bf6e7e7b963babde65f31aa90d727a16d30f
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-11-24 13:14:45 -0800

    first round of fixes

---
+++ server/src/__tests__/classroomService.test.ts
@@ -0,0 +1,70 @@
+import mockingoose from 'mockingoose';
+import { ClassroomModel, UserModel } from '../database';
+import { classRoomDoc4, classRoomDoc5, user1 } from '../database/mockData';
+import {
+    authenticateToClassroom,
+    createClassroom,
+    getStudents,
+} from '../service/classroomService';
+
+test('Check if createClassroom creates classroom correctly', async () => {
+    mockingoose(ClassroomModel).toReturn(
+        { name: 'Test100', passcode: '12345', problemsForToday: [] },
+        'save',
+    );
+    try {
+        const classRoom = await createClassroom(classRoomDoc4);
+        expect(JSON.parse(JSON.stringify(classRoom))).toMatchObject(
+            classRoomDoc4,
+        );
+    } catch (e) {
+        expect(e).toEqual(
+            'A classroom already exists with this given name and passcode',
+        );
+    }
+});
+
+test('Check if authenticateToClassroom returns classroomId properly', async () => {
+    mockingoose(ClassroomModel).toReturn(
+        {
+            _id: '5dd0ddeaa1608611fdb1bb40',
+            name: 'Test100',
+            passcode: '12345',
+            problemsForToday: [],
+        },
+        'findOne',
+    );
+    try {
+        const classRoomId = await authenticateToClassroom(classRoomDoc4);
+        expect(classRoomId).toEqual('5dd0ddeaa1608611fdb1bb40');
+    } catch (e) {
+        expect(e).toEqual('Invalid classroom name or passcode');
+    }
+});
+
+test('Check if authenticateToClassroom validates classroom properly - Should throw an error', async () => {
+    mockingoose(ClassroomModel).toReturn(
+        {
+            _id: '5dd0ddeaa1608611fdb1bb40',
+            name: 'Test100',
+            passcode: '12345',
+            problemsForToday: [],
+        },
+        'findOne',
+    );
+    try {
+        const classRoomId = await authenticateToClassroom(classRoomDoc5);
+        expect(classRoomId).toEqual('5dd0ddeaa1608611fdb1bb40');
+    } catch (e) {
+        const err = JSON.parse(JSON.stringify(e));
+        expect(err.output.payload.message).toEqual(
+            'Invalid classroom name or passcode',
+        );
+    }
+});
+
+test('Check if getStudents returns students correctly', async () => {
+    mockingoose(UserModel).toReturn(user1, 'find');
+    const students = await getStudents('113015909143620944320');
+    expect(JSON.parse(JSON.stringify(students))).toMatchObject(user1);
+});
---
+++ server/src/database/mockData.ts
@@ -0,0 +1,164 @@
+import { IAuthInfo } from '@server/service/authService';
+import { IClassroom } from '@shared/models/classroom';
+import { IUser } from '@shared/models/users';
+
+export const user1: IUser = {
+    id: '113015909143620944320',
+    registered: true,
+    userType: 'student',
+    virtualClassroomUid: '113015909143620944320',
+};
+
+export const user2: IUser = {
+    email: 'test@gmail.com',
+    id: '1122334455667788',
+    name: 'testUser',
+    registered: false,
+    userType: 'student',
+    virtualClassroomUid: '113015909143620944320',
+};
+
+export const user3: IUser = {
+    email: 'test@gmail.com',
+    id: '1122334455667788',
+    registered: false,
+};
+
+export const user5: IUser = {
+    email: 'test@gmail.com',
+    id: '1122334455667788',
+    name: 'testUser',
+    pushToken: '123456789987654321',
+    registered: true,
+    userType: 'student',
+    virtualClassroomUid: '113015909143620944320',
+};
+
+export const masteryDoc1 = {
+    _id: '5dcfa29372e21e4adca02e93',
+    progress: {
+        addition: {
+            _id: '5dcfa29372e21e4adca02e94',
+            currentDifficultyAttempts: 9,
+            currentDifficultyPoints: 4,
+            difficulty: 'g1h2e',
+            totalPoints: 123,
+        },
+        division: {
+            _id: '5dcfa2aa72e21e4adca02e32',
+            currentDifficultyAttempts: 0,
+            currentDifficultyPoints: 0,
+            difficulty: 'g4m',
+            totalPoints: 90,
+        },
+        equation: {
+            _id: '5dcfa2aa72e21e4adca02e32',
+            currentDifficultyAttempts: 0,
+            currentDifficultyPoints: 0,
+            difficulty: 'g3h4e',
+            totalPoints: 90,
+        },
+        multiplication: {
+            _id: '5dcfa2aa72e21e4adca02ea2',
+            currentDifficultyAttempts: 3,
+            currentDifficultyPoints: 9,
+            difficulty: 'g1h2e',
+            totalPoints: 90,
+        },
+        subtraction: {
+            _id: '5dcfa2aa72e21e4adca02ea1',
+            currentDifficultyAttempts: 0,
+            currentDifficultyPoints: 0,
+            difficulty: 'g5m',
+            totalPoints: 90,
+        },
+    },
+    studentId: '113015909143620944320',
+};
+
+export const masteryDoc2 = {
+    _id: '5dcfa29372e21e4adca02e93',
+    progress: {
+        addition: {
+            _id: '5dcfa29372e21e4adca02e94',
+            currentDifficultyAttempts: 23,
+            currentDifficultyPoints: 23,
+            difficulty: 'g1h2e',
+            totalPoints: 123,
+        },
+        division: {
+            _id: '5dcfa2aa72e21e4adca02e32',
+            currentDifficultyAttempts: 9,
+            currentDifficultyPoints: 5,
+            difficulty: 'g1m',
+            totalPoints: 90,
+        },
+        equation: {
+            _id: '5dcfa2aa72e21e4adca02e32',
+            currentDifficultyAttempts: 8,
+            currentDifficultyPoints: 5,
+            difficulty: 'g1m',
+            totalPoints: 90,
+        },
+        multiplication: {
+            _id: '5dcfa2aa72e21e4adca02ea2',
+            currentDifficultyAttempts: 10,
+            currentDifficultyPoints: 5,
+            difficulty: 'g1m',
+            totalPoints: 90,
+        },
+        subtraction: {
+            _id: '5dcfa2aa72e21e4adca02ea1',
+            currentDifficultyAttempts: 11,
+            currentDifficultyPoints: 5,
+            difficulty: 'g1m',
+            totalPoints: 90,
+        },
+    },
+    studentId: '113015909143620944320',
+};
+
+export const classRoomDoc1 = {
+    _id: '113015909143620944320',
+    name: 'Test101',
+    passcode: '12345',
+    problemsForToday: ['addition', 'subtraction'],
+};
+
+export const classRoomDoc2 = {
+    _id: '113015909143620944320',
+    name: 'Test101',
+    passcode: '12345',
+    problemsForToday: [],
+};
+
+export const classRoomDoc3 = {
+    _id: '113015909143620944320',
+    name: 'Test101',
+    passcode: '12345',
+    problemsForToday: ['addition', 'volumes'],
+};
+
+export const classRoomDoc4: IClassroom = {
+    name: 'Test100',
+    numDailyProblems: 20,
+    onlineResources: '',
+    passcode: '12345',
+    problemsForToday: [],
+};
+
+export const classRoomDoc5: IClassroom = {
+    name: 'Test105',
+    numDailyProblems: 20,
+    onlineResources: '',
+    passcode: '11122',
+    problemsForToday: ['addition'],
+};
+
+export const authDoc1: IAuthInfo = {
+    aud: 'Test',
+    email: 'test@gmail.com',
+    email_verified: true,
+    name: 'testUser',
+    sub: 1122334455667788,
+};
